8baf72adc2bcc4ff303ea1efba066c6ce54083b5
Sohyeon <81665465+sohyeondian@users.noreply.github.com>
2021-04-22 13:36:13 +0900

    Update max_num.py

--- max_num.py
+++ max_num.py
@@ -1,10 +1,8 @@
 def solution(numbers):
-<<<<<<< HEAD
     
     answer = ''
     return answer
 
-=======
     numbers = list(map(str, numbers))
 
     nums = quick(numbers)
@@ -31,23 +29,20 @@ def quick(arr):
 print(solution([101, 1, 10]))  # 110110 101 > 10
 print(solution([110, 100, 111, 1000]))
 print(solution([212, 21, 2]))
->>>>>>> a1f29a2 (Solved max_num.py)
 
 print(solution([0, 0, 0]))
 print(solution([3, 30, 34, 5, 9]))
 print(solution([21, 212]))
-<<<<<<< HEAD
+
 
 print(solution([34, 3, 0]))  # 3430
 print(solution([31, 3, 2]))  # 3312
 
 print(sorted(['31', '3', '2'], reverse=True))
 print('31' > '3')
-=======
+
 print(solution([90,908,89,898,10,101,1,8,9]))
 print(solution([1, 11, 111, 1111] ))
 print(solution([10, 101]))
 print(solution([0, 0, 0, 0, 0, 0]))
 print(solution([8, 898, 89])) # 898988
->>>>>>> a1f29a2 (Solved max_num.py)
-
